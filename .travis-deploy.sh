c49249d3e8a096be90daab95e74790e67b467dc2
Stuart Mumford <stuart@cadair.com>
2016-12-19 14:33:42 +0000

    only deploy on linux

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "$TRAVIS_OS_NAME" = "linux" ]]; then
     openssl aes-256-cbc -K $encrypted_b62220aba756_key -iv $encrypted_b62220aba756_iv -in publish-key.enc -out ~/.ssh/publish-key -d
     chmod u=rw,og= ~/.ssh/publish-key
     echo "Host github.com" >> ~/.ssh/config
